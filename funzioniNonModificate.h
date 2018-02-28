4eb5b8ac537a85dc6252daf43bd2dd55bb5b6191
Filippo Calabrese <filippo.calabrese@stud.unifi.it>
2018-02-28 18:12:36 +0100

    Ripulite altre funzioni

--- funzioniNonModificate.h
+++ funzioniNonModificate.h
@@ -40,6 +40,13 @@ typedef struct fn_actor_redball_lying_data_t {
   int touching_hero;
 } fn_actor_redball_lying_data_t;
 
+struct fn_item_t {
+  int pixelsize;
+  int x;
+  int y;
+  fn_level_t * level;
+  int todelete;
+};
 
 struct fn_hero_t {
   int direction;
@@ -64,6 +71,39 @@ struct fn_hero_t {
   //fn_list_t * hurtingactors;
 };
 
+enum fn_item_type_e {
+  FN_ITEM_TYPE_BOX_GREY_EMPTY,
+  FN_ITEM_TYPE_BOX_GREY_BOOTS,
+  FN_ITEM_TYPE_BOX_GREY_CLAMPS,
+  FN_ITEM_TYPE_BOX_GREY_GUN,
+  FN_ITEM_TYPE_BOX_GREY_BOMB,
+  FN_ITEM_TYPE_BOX_RED_SODA,
+  FN_ITEM_TYPE_BOX_RED_CHICKEN,
+  FN_ITEM_TYPE_BOX_BLUE_FOOTBALL,
+  FN_ITEM_TYPE_BOX_BLUE_JOYSTICK,
+  FN_ITEM_TYPE_BOX_BLUE_DISK,
+  FN_ITEM_TYPE_BOX_GREY_GLOVE,
+  FN_ITEM_TYPE_BOX_BLUE_BALLOON,
+  FN_ITEM_TYPE_BOX_GREY_FULL_LIFE,
+  FN_ITEM_TYPE_BOX_BLUE_FLAG,
+  FN_ITEM_TYPE_BOX_BLUE_RADIO,
+  FN_ITEM_TYPE_BOX_GREY_ACCESS_CARD,
+  FN_ITEM_TYPE_BOX_GREY_D,
+  FN_ITEM_TYPE_BOX_GREY_U,
+  FN_ITEM_TYPE_BOX_GREY_K,
+  FN_ITEM_TYPE_BOX_GREY_E,
+  FN_ITEM_TYPE_FOOTBALL,
+  FN_ITEM_TYPE_CHICKEN_SINGLE,
+  FN_ITEM_TYPE_CHICKEN_DOUBLE,
+  FN_ITEM_TYPE_SODA,
+  FN_ITEM_TYPE_SODA_FLYING,
+  FN_ITEM_TYPE_DISK,
+  FN_ITEM_TYPE_JOYSTICK,
+  FN_ITEM_TYPE_FLAG,
+  FN_ITEM_TYPE_RADIO
+};
+
+
 typedef struct fn_actor_item_data_t {
   int tile;
   int current_frame;
@@ -77,15 +117,7 @@ typedef struct fn_actor_singleanimation_data_t {
   int num_frames;
 } fn_actor_singleanimation_data_t;
 
-#ifndef FN_LIST_H
-#define FN_LIST_H
-
 typedef struct fn_list_t fn_list_t;
-
-struct fn_list_t {
-  fn_list_t * next;
-  void * data;
-};
 
 int fn_actor_touches_hero(fn_actor_t * actor);
 int fn_hero_get_fetched_letter(fn_hero_t * hero);
@@ -96,3 +128,6 @@ void fn_hero_add_score(fn_hero_t * hero, int score);
 void fn_actor_function_redball_lying_act(fn_actor_t * actor, int x);
 void fn_actor_function_item_touch_start(fn_actor_t * actor);
 fn_hero_t * fn_level_get_hero(fn_level_t * lv);
+fn_list_t * fn_list_first(fn_list_t * list);
+fn_list_t * fn_list_last(fn_list_t * list);
+fn_list_t * fn_list_next(fn_list_t * list);
